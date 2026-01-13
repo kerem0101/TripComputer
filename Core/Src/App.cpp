430383524b2b8db87f056a8c666ac481699a90a5
Aziz Kerem Demir <demirazizkerem@gmail.com>
2026-01-14 00:06:58 +0300

    freertos and task management were added

--- Core/Src/App.cpp
+++ Core/Src/App.cpp
@@ -1,11 +1,20 @@
 #include "App.h"
 #include "main.h" // for HAL library access
-#include "bsp_board.h"
+#include "bsp_board.h" // for BSP functions
+#include "SystemTask.hpp" // for SystemTask class
+
+SystemTask systemTask;
 
 void App_Main() {
-    while(1) {
-        
-        BSP_LED_Toggle(LED_SYSTEM_STATUS);
-        BSP_Delay(500);
+    
+    
+    bool status = systemTask.start();
+
+    if (!status) {
+        // Handle task creation failure (optional)
+        while (1) {
+            // Stay here if task creation failed
+
+        }
     }
 }
